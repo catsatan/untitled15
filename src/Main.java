93c305f338178ef18e5772147f1bb1cedb1476c9
admin <catsatan>
2022-06-16 19:59:49 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int salary =6565656;
+        int total = 0;
+                for (int i = 0; i<=12 ;i++  ){
+                    total=total+total/100;
+                    total = total+salary;
+        }
+        System.out.println(total);
+
+        }
+    }
